ed16e6189a6ef0e29136f0a7e6c218ca4063acb4
Christopher Coe <christopherdcoe@gmail.com>
2018-08-15 01:33:57 -0500

    Changed Orc and Maakiin to Gnoll and Kobold, changed racial ability score modifiers

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Tab navigation widget
+$( function() {
+  $( "#tabs" ).tabs();
+  $( "#tabs2" ).tabs();
+} );
+
+$( function() {
+  $( "#accordion" ).accordion({
+    heightStyle: "content",
+    collapsible: true,
+    active: 0
+  });
+} );
